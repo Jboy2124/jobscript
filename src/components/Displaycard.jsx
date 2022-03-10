10f35a05e9637e41108e57bb196af3187aed7aa2
Jufel Abecia <jufelabecia@gmail.com>
2022-03-10 22:41:09 +0800

    adding card

--- src/components/Displaycard.jsx
+++ src/components/Displaycard.jsx
@@ -1,20 +1,19 @@
 import React from 'react'
 import JobSeeker from '../images/home-images/hero-img-1.jpg'
-import Employer from '../images/home-images/hero-img-2.jpg'
-import { Card, CardActions, CardContent, CardMedia, Button, Typography } from '@mui/material';
+// import Employer from '../images/home-images/hero-img-2.jpg'
 
 const Displaycard = (props) => {
   return (
-    <Card className='cards' sx={{ width: 280, boxShadow: "0px 0px 2px 1px #e63c3c" }}>
-        <CardMedia component="img" alt='' height="200" image={JobSeeker}/>
-        <CardContent>
-            <Typography gutterBottom variant="h5" component="div">{props.title}</Typography>
-            <Typography variant="body2" color="text.secondary">{props.description}</Typography>
-        </CardContent>
-        <CardActions>
-            <Button>See more</Button>
-        </CardActions>
-    </Card>
+    <div className="container-fluid">
+      <div className="card">
+        <img src={JobSeeker} alt="Job Seeker" className="card-img-top" />
+        <div className="card-body">
+          <h5 className="card-title">{props.title}</h5>
+          <p className="card-text">{props.description}</p>
+          <a href="#" className="btn btn-primary">Show more</a>
+        </div>
+      </div>
+    </div>
   )
 }
 
--- src/pages/TheDeveloper.jsx
+++ src/pages/TheDeveloper.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
-
+import '../css/developer.css'
+import Cards from '../components/Displaycard'
+import Datalist from '../data-model/DataList'
 
 const TheDeveloper = () => {
   return (
-    <div className='container text-center'>
-      <h1>Hello</h1>
+    <div id='card-container' className='container'>
+      <div className='dev-cards'>
+      {Datalist.map((items) => {
+          return(<Cards title={items.title} description={items.description}/>)
+        })}
+      </div>
     </div>
   )
 }
