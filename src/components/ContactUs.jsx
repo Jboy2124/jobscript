767accb936235f26d14ba091ace282123628b2c6
Jufel Abecia <jufelabecia@gmail.com>
2022-03-13 16:16:17 +0800

    adding footer

--- src/components/ContactUs.jsx
+++ src/components/ContactUs.jsx
@@ -6,7 +6,7 @@ const ContactUs = () => {
     <div id='contact-us-container' className='container'>
         <h1>Contact Us</h1>
         <h2 className='text-center'>Thank you for visiting JobScript</h2>
-        <p className='text-center'>Whether you are a job seeker looking for tips on using JobScript, 
+        <p className='text-center pb-5'>Whether you are a job seeker looking for tips on using JobScript, 
                     or an employer interested in our  products and services,
                     we want to hear from you! Please choose from the following 2 options</p>
 
