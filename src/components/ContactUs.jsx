3fffdc644531ee8f68ee1ae1564f16cfa6add97e
Elizabeth Santos <ECSantos1990@gmail.com>
2022-03-15 20:50:09 +0800

    made the log in forms dynamic and edited the footer responsiveness

--- src/components/ContactUs.jsx
+++ src/components/ContactUs.jsx
@@ -4,33 +4,35 @@ import '../css/contactus.css'
 const ContactUs = () => {
   return (
     <div id='contact-us-container' className='container'>
-        <h1>Contact Us</h1>
+        <h2 className="serviceHeading">Contact Us</h2>
         <h2 className='text-center'>Thank you for visiting JobScript</h2>
-        <p className='text-center pb-5'>Whether you are a job seeker looking for tips on using JobScript, 
-                    or an employer interested in our  products and services,
-                    we want to hear from you! Please choose from the following 2 options</p>
+        <p className='description text-center pb-5'>Whether you are a job seeker looking for tips on using JobScript, 
+                    or an employer interested in our <br/>  products and services,
+                    we want to hear from you! Please choose from the following 2 options.</p>
 
-        <div className="row">
-            <div className="col-sm-6 border py-3">
-                <h3>Job Seeker</h3>
-                <p>if you are a employer and would like to contact us, call us at</p>
-                <p>Toll Free: 1-80-011-1001-060</p>
-                <p>*Toll: 0091-40-06118964</p>
-                <p>(Standard STD/ charges apply)</p>
-                <p>*Press 3 when the IVR messages comes up</p>
-                <p>To reach us through mail, click the button below.</p>
-                <button type='button' className='btn btn-outline-danger' >Message Us</button>
-            </div>
-            <div className="col-sm-6 border py-3">
-            <h3>Looking for Talent</h3>
-                <p>if you are a job seeker and would like to contact us, call us at</p>
-                <p>Toll Free: 1-80-011-1001-060</p>
-                <p>*Toll: 0091-40-06118964</p>
-                <p>(Standard STD/ charges apply)</p>
-                <p>*Press 1 when the IVR messages comes up</p>
-                <p>To reach us through mail, click the button below.</p>
-                <button type='button' className='btn btn-outline-danger' >Message Us</button>
-            </div>
+        <div className="row  ">
+          <div className="container justify-content-center d-flex flexColumn">
+              <div className="col-sm-5 border p-3 width">
+                  <p className='serviceTopicEmployer fontSmaller'> Job Seeker </p>
+                  <p>If you are a job seeker and would like to contact us, call us at</p>
+                  <p>Toll Free: 1-80-011-1001-060</p>
+                  <p>*Toll: 0091-40-06118964</p>
+                  <p>(Standard STD/ charges apply)</p>
+                  <p>*Press 3 when the IVR messages comes up</p>
+                  <p>To reach us through mail, click the button below.</p>
+                  <button type='button' className='btn btn-outline-danger' >Message Us</button>
+              </div>
+              <div className="col-sm-5 border p-3 width">
+              <p className= 'serviceTopicEmployer fontSmaller'>Employer </p>
+                  <p>If you are a employer and would like to contact us, call us at</p>
+                  <p>Toll Free: 1-80-011-1001-060</p>
+                  <p>*Toll: 0091-40-06118964</p>
+                  <p>(Standard STD/ charges apply)</p>
+                  <p>*Press 1 when the IVR messages comes up</p>
+                  <p>To reach us through mail, click the button below.</p>
+                  <button type='button' className='btn btn-outline-danger' >Message Us</button>
+              </div>
+          </div>
         </div>
     </div>
   )
