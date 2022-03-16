6308d68de42303d0f4d11fe761c79d7f25dccb20
Jufel Abecia <jufelabecia@gmail.com>
2022-03-16 11:10:38 +0800

    finalizing jobseekerdash

--- src/components/JobSeekerDash.jsx
+++ src/components/JobSeekerDash.jsx
@@ -1,12 +1,11 @@
 import React, { useState, useEffect } from 'react'
-import { Offcanvas } from 'react-bootstrap'
+import { Offcanvas, Modal, Button } from 'react-bootstrap'
 import '../css/jobseekerdash.css'
 import JobSeekerDashCards from './JobSeekerDashCards';
 import JobList from '../data-model/JobList';
 import Footer from './Footer';
-import DisplayModal from './DisplayModal';
-
-
+let salaryValue = 0;
+let arrayJobDetails = [];
 
 
 
@@ -16,19 +15,14 @@ const JobSeekerDash = () => {
   const handleShow = () => setShow(true);
   const [showModal, setShowModal] = useState(false);
   
+  let newArray = [];
   
-  const handlesEvent = (passedID) => {
-    
-    for (const key in JobList) {
-      if (JobList[key].id == passedID) {
-        setShowModal(true);
-        // alert("Full Details: JobID: " + JobList[key].id +" "+ JobList[key].category +" "+ JobList[key].jobTitle +" "+ JobList[key].jobSnippet)
-      }
-    }  
+  
+
+  const handlesShowModal = (clickedCardValue) => {
+    setShowModal(clickedCardValue[1]);
+    arrayJobDetails = JobList.filter(item => item.id == clickedCardValue[0]).map(list => { return list });
   }
-
-
-  // let handleModalEvent = () => setShowModal(true);
 
     return (
       <div className="container-fluid p-0">
@@ -41,7 +35,7 @@ const JobSeekerDash = () => {
                 JobList.map((items) => {
                   return (
                     <div id='display-card-columns' className="col">
-                      <JobSeekerDashCards handlesJobClickEvent={handlesEvent} id={items.id} category={items.category} title={items.jobTitle} snippets={items.jobSnippet}/>
+                      <JobSeekerDashCards handlesShowModal={handlesShowModal} id={items.id} category={items.category} title={items.jobTitle} snippets={items.jobSnippet} showData={true}/>
                     </div>
                   )
                 })
@@ -53,14 +47,52 @@ const JobSeekerDash = () => {
           </div>
 
 
-          <DisplayModal showModal={showModal} />
-        
-          
 
+            { arrayJobDetails.map((items) => {
+                salaryValue = parseFloat(items.salary).toLocaleString(undefined, {maximumFractionDigits: 2});
+                return (
+                  <Modal show={showModal} size="lg" aria-labelledby="contained-modal-title-vcenter" centered >
+                    <Modal.Header className='modal-header'>
+                      <Modal.Title className='modal-title'>ABOUT THE JOB IN DETAILS</Modal.Title>
+                      <div className='modal-tag'>
+                        <p>{items.category}</p>
+                      </div>
+                    </Modal.Header>
+                      <Modal.Body className='modal-body'>
+                        <h5>{items.jobTitle}</h5>
+                        <p className='modal-body-company' >{items.company}</p>
+                        <p className='modal-body-address'>{items.address}</p>
+                        <p className='modal-body-contact'>Contact: {items.ein} / Email: {items.companyEmail}</p>
+                        <p className='modal-body-salary'>Salary: {(items.salary) ? salaryValue : "Not Specified"}</p>
+                        <p className='modal-body-aor'>{items.aor}</p>
+                        <hr />
+                        <h6 className='modal-qualification-header'>Job Overview</h6>
+                          <ul className='modal-requirements'>
+                            {
+                              items.jobDescription.map(i => {
+                                return ( <li>{i}</li> )
+                              })
+                            }
+                          </ul>
 
-     
-
-          
+                          <h6 className='modal-qualification-header'>Skills Summary</h6>
+                          <ul className='modal-requirements'>
+                            {
+                              items.skills.map(s => {
+                                return (<li>{s}</li> )
+                              })
+                            }
+                          </ul>
+                      </Modal.Body>
+                    <Modal.Footer>
+                      {/* <Button className='btn btn-outline-danger' onClick={() => setShowModal(false)} >Close</Button></Modal.Footer> */}
+                      <button className='btn btn-outline-danger' onClick={() => setShowModal(false)}>Close</button>
+                      </Modal.Footer>
+                  </Modal> 
+                )
+              })
+            }
+                   
          {/* <Offcanvas show={show} onHide={handleClose}>
             <Offcanvas.Header closeButton>
               <Offcanvas.Title>Offcanvas</Offcanvas.Title>
